0caa1cc6dee60739490106c3ce3541e8a0c3dbfa
Monica Liang <monicaliang@rocketmail.com>
2020-12-31 16:07:32 -0500

    completed some of phase 1

--- twitter/frontend/follow_toggle.js
+++ twitter/frontend/follow_toggle.js
@@ -1,8 +1,42 @@
 class FollowToggle{
     constructor($el){
-        this.userId = $el.data('user-id');
-        this.followState = $el.data('initial-follow-state');
         const $button = $('$el')
+        this.userId = $button.data('user-id');
+        this.followState = $button.data('initial-follow-state');
+        this.render();
+    }
+
+    render() {
+        if (this.followState === "Not Followed") {
+            $button.text("Follow!");
+        } else {
+            $button.text("Unfollow!");
+        }
+    }
+
+    handleClick() {
+        $button.on("click", e => {
+            e.preventDefault();
+            if (this.followState === "Not Followed") {
+                $.ajax({
+                    method: "POST",
+                    url: "/users/:id/follow",
+                    dataType: json,
+                    success () {
+                        this.followState = "Followed";
+                    }
+                })
+            } else {
+                $.ajax({
+                    method: "DELETE",
+                    url: "/users/:id/follow",
+                    dataType: json,
+                    success () {
+                        this.followState = "Not Followed";
+                    }
+                })
+            }  
+        })
     }
 
 }
--- twitter/frontend/twitter.js
+++ twitter/frontend/twitter.js
@@ -1,1 +1,7 @@
 const FollowToggle = require ("./follow_toggle.js")
+
+$(() => {
+    $("button.follow-toggle").each(el => {
+        new FollowToggle(el);
+    });
+})
